e061034f6174a2b014a849be4e7b6d92f1e7c061
UcenElma <elma.hocine@gmail.com>
2023-12-30 17:17:30 +0100

    two particles moving in the same line and fusing to create a new particle

--- physic/Particales.py
+++ physic/Particales.py
@@ -1,76 +1,78 @@
 import pygame
+import sys
 import random
 import math
 
-# Initialize Pygame
-pygame.init()
-
 # Constants
 WIDTH, HEIGHT = 800, 600
-PARTICLE_RADIUS = 5
-NUM_PARTICLES = 100
-FPS = 60
-
-# Colors
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
+PARTICLE_RADIUS = 20
+SPEED = 5
+FUSION_DISTANCE = 2 * PARTICLE_RADIUS
 
 # Particle class
 class Particle:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
-        self.radius = PARTICLE_RADIUS
-        self.color = WHITE
-        self.velocity = [random.uniform(-2, 2), random.uniform(-2, 2)]
+        self.angle = 0  # Move in the same line
+        self.color = color
 
     def update(self):
-        self.x += self.velocity[0]
-        self.y += self.velocity[1]
+        self.x += SPEED * math.cos(self.angle)
+        self.y += SPEED * math.sin(self.angle)
 
-        # Collisions with walls
-        if self.x - self.radius < 0 or self.x + self.radius > WIDTH:
-            self.velocity[0] *= -1
-        if self.y - self.radius < 0 or self.y + self.radius > HEIGHT:
-            self.velocity[1] *= -1
+        # Collision with walls
+        if self.x <= PARTICLE_RADIUS or self.x >= WIDTH - PARTICLE_RADIUS:
+            self.angle = math.pi - self.angle
+        if self.y <= PARTICLE_RADIUS or self.y >= HEIGHT - PARTICLE_RADIUS:
+            self.angle = -self.angle
 
-    def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
+def distance(particle1, particle2):
+    return math.sqrt((particle1.x - particle2.x)**2 + (particle1.y - particle2.y)**2)
 
-# Initialize screen
+# Initialize Pygame
+pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Particle Simulation")
+pygame.display.set_caption("Particle Fusion Simulation")
 
-# Create particles
-particles = [Particle(random.randint(PARTICLE_RADIUS, WIDTH - PARTICLE_RADIUS),
-                      random.randint(PARTICLE_RADIUS, HEIGHT - PARTICLE_RADIUS))
-             for _ in range(NUM_PARTICLES)]
+# Create two particles
+particle1 = Particle(WIDTH // 4, HEIGHT // 2, (255, 0, 0))  # Red
+particle2 = Particle(3 * WIDTH // 4, HEIGHT // 2, (0, 0, 255))  # Blue
 
 # Main loop
+running = True
 clock = pygame.time.Clock()
 
-running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     # Update particles
-    for particle in particles:
-        particle.update()
+    particle1.update()
+    particle2.update()
 
-    # Draw background
-    screen.fill(RED)
+    # Check for collision
+    if distance(particle1, particle2) < FUSION_DISTANCE:
+        # Create a new particle at the midpoint of collision with a new color
+        new_particle_x = (particle1.x + particle2.x) // 2
+        new_particle_y = (particle1.y + particle2.y) // 2
+        new_particle_color = ((particle1.color[0] + particle2.color[0]) // 2,
+                              (particle1.color[1] + particle2.color[1]) // 2,
+                              (particle1.color[2] + particle2.color[2]) // 2)
+        new_particle = Particle(new_particle_x, new_particle_y, new_particle_color)
 
-    # Draw particles
-    for particle in particles:
-        particle.draw(screen)
+        # Draw particles
+        screen.fill((255, 255, 255))  # White background
+        pygame.draw.circle(screen, new_particle.color, (int(new_particle.x), int(new_particle.y)), PARTICLE_RADIUS)
+    else:
+        # Draw particles
+        screen.fill((255, 255, 255))  # White background
+        pygame.draw.circle(screen, particle1.color, (int(particle1.x), int(particle1.y)), PARTICLE_RADIUS)
+        pygame.draw.circle(screen, particle2.color, (int(particle2.x), int(particle2.y)), PARTICLE_RADIUS)
 
-    # Update display
     pygame.display.flip()
+    clock.tick(60)
 
-    # Cap the frame rate
-    clock.tick(FPS)
-
-# Quit Pygame
 pygame.quit()
+sys.exit()
